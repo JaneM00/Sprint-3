8675f4c28037f743063aab48f94314d1f2fe23f7
JaneM00 <evg.mamikonyan@yandex.ru>
2025-04-27 00:05:10 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
+# conftest.py
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def browser():
-    """Инициализирует веб-драйвер и закрывает его после завершения сессии."""
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
     browser = webdriver.Chrome(options=chrome_options)
