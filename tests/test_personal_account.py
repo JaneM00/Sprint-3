67ebaf21e0b33ee96df834a33025d7deb3a89e64
JaneM00 <evg.mamikonyan@yandex.ru>
2025-05-01 00:48:35 +0300

    Update test_personal_account.py

--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -1,13 +1,18 @@
 # tests/test_personal_account.py
 
-import pytest
+import time
+import unittest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import *
-from helpers import perform_login
 
-@pytest.mark.usefixtures("browser")
-class TestPersonalAccount:
+class TestPersonalAccount(unittest.TestCase):
+
+    def setUp(self):
+        pass  # Всё управление браузером передано в фикстуру
+
+    def tearDown(self):
+        pass  # Всё управление браузером передано в фикстуру
 
     # Тест перехода в личный кабинет по ссылке "Личный кабинет"
     def test_personal_account_link_click(self, browser):
@@ -15,10 +20,12 @@ class TestPersonalAccount:
         browser.find_element(*PERSONAL_ACCOUNT_LINK).click()
         wait = WebDriverWait(browser, 10)
         wait.until(EC.visibility_of_element_located(LOGIN_FORM_EMAIL_INPUT))  # Появилась форма авторизации
-        perform_login(browser, 'existing_user@example.com', 'valid_password')
+        browser.find_element(*LOGIN_FORM_EMAIL_INPUT).send_keys('existing_user@example.com')
+        browser.find_element(*LOGIN_FORM_PASSWORD_INPUT).send_keys('valid_password')
+        browser.find_element(*LOGIN_FORM_SUBMIT_BUTTON).click()
         wait.until(EC.visibility_of_element_located(PERSONAL_ACCOUNT_LINK))
         personal_account_text = browser.find_element(*PERSONAL_ACCOUNT_LINK).text
-        assert personal_account_text == 'Личный кабинет'
+        self.assertEqual(personal_account_text, 'Личный кабинет')
 
     # Тест выхода из аккаунта
     def test_logout(self, browser):
@@ -26,9 +33,11 @@ class TestPersonalAccount:
         browser.find_element(*PERSONAL_ACCOUNT_LINK).click()
         wait = WebDriverWait(browser, 10)
         wait.until(EC.visibility_of_element_located(LOGIN_FORM_EMAIL_INPUT))  # Появилась форма авторизации
-        perform_login(browser, 'existing_user@example.com', 'valid_password')
+        browser.find_element(*LOGIN_FORM_EMAIL_INPUT).send_keys('existing_user@example.com')
+        browser.find_element(*LOGIN_FORM_PASSWORD_INPUT).send_keys('valid_password')
+        browser.find_element(*LOGIN_FORM_SUBMIT_BUTTON).click()
         wait.until(EC.visibility_of_element_located(PERSONAL_ACCOUNT_LINK))
         browser.find_element(*PERSONAL_ACCOUNT_LOGOUT_BUTTON).click()
         wait.until(EC.invisibility_of_element_located(PERSONAL_ACCOUNT_LINK))  # Личный кабинет исчез
         logout_success_text = browser.find_element(*LOGIN_BUTTON_MAIN_PAGE).text
-        assert logout_success_text == 'Войти в аккаунт'
+        self.assertEqual(logout_success_text, 'Войти в аккаунт')
