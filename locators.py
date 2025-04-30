cfecd516abf7df743987fa7d5075ce352af41b35
JaneM00 <evg.mamikonyan@yandex.ru>
2025-05-01 00:43:39 +0300

    Update locators.py

--- locators.py
+++ locators.py
@@ -2,12 +2,12 @@
 
 from selenium.webdriver.common.by import By
 
-# Локаторы для главной страницы
+# Главное меню
 MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/'
+BURGER_LOGO = (By.CSS_SELECTOR, '.AppHeader_header__logo__2TDzT')
 LOGIN_BUTTON_MAIN_PAGE = (By.CSS_SELECTOR, 'a[href="/login"]')
 REGISTER_BUTTON_MAIN_PAGE = (By.CSS_SELECTOR, 'a[href="/register"]')
 PERSONAL_ACCOUNT_LINK = (By.CSS_SELECTOR, '.AppHeader_header__linkText__bZTlX.AppHeader_header__link__1f_O6')
-BURGER_LOGO = (By.CSS_SELECTOR, '.AppHeader_header__logo__2TDzT')
 
 # Формы входа и регистрации
 LOGIN_FORM_EMAIL_INPUT = (By.NAME, 'email')
@@ -22,13 +22,10 @@ REGISTRATION_FORM_SUBMIT_BUTTON = (By.CSS_SELECTOR, 'button[type="submit"]')
 # Личный кабинет
 PERSONAL_ACCOUNT_LOGOUT_BUTTON = (By.CSS_SELECTOR, '.profile-form__exit')
 
-# Конструктор бургера
+# Конструктор
 CONSTRUCTOR_BUNS_SECTION = (By.ID, 'buns')
 CONSTRUCTOR_SAUCE_SECTION = (By.ID, 'sauces')
 CONSTRUCTOR_FILLINGS_SECTION = (By.ID, 'fillings')
 
-# Таб навигации
-NAVIGATION_ACTIVE_TAB = (By.CSS_SELECTOR, '.tab.active')
-
-# Сообщения об ошибках
-ERROR_MESSAGE = (By.CLASS_NAME, 'error-message')
+# Элементы активного состояния (для проверок)
+ACTIVE_TAB_CLASS = 'current'
