f07f35c17301b622a9f4d454f83ee3cc9edb5ef0
JaneM00 <evg.mamikonyan@yandex.ru>
2025-05-05 12:36:08 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -12,11 +12,3 @@ def browser():
     browser = webdriver.Chrome(options=chrome_options)
     yield browser
     browser.quit()
-
-@pytest.fixture(scope='session')
-def browser():
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    browser = webdriver.Chrome(options=chrome_options)
-    yield browser
-    browser.quit()
