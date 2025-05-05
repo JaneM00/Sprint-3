3b75ecc0436288ba47e6909a1c354d1e4e7cdb50
JaneM00 <evg.mamikonyan@yandex.ru>
2025-05-05 13:25:05 +0300

    Update test_personal_account.py

--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -1,18 +1,19 @@
 # tests/test_personal_account.py
 
-import time
-import unittest
+import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import *
 
-class TestPersonalAccount(unittest.TestCase):
+@pytest.fixture(scope='session')
+def browser():
+    """Инициализируем и возвращаем экземпляр браузера."""
+    from selenium import webdriver
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
 
-    def setUp(self):
-        pass  # Всё управление браузером передано в фикстуру
-
-    def tearDown(self):
-        pass  # Всё управление браузером передано в фикстуру
+class TestPersonalAccount:
 
     # Тест перехода в личный кабинет по ссылке "Личный кабинет"
     def test_personal_account_link_click(self, browser):
@@ -25,7 +26,7 @@ class TestPersonalAccount(unittest.TestCase):
         browser.find_element(*LOGIN_FORM_SUBMIT_BUTTON).click()
         wait.until(EC.visibility_of_element_located(PERSONAL_ACCOUNT_LINK))
         personal_account_text = browser.find_element(*PERSONAL_ACCOUNT_LINK).text
-        self.assertEqual(personal_account_text, 'Личный кабинет')
+        assert personal_account_text == 'Личный кабинет'
 
     # Тест выхода из аккаунта
     def test_logout(self, browser):
@@ -40,4 +41,4 @@ class TestPersonalAccount(unittest.TestCase):
         browser.find_element(*PERSONAL_ACCOUNT_LOGOUT_BUTTON).click()
         wait.until(EC.invisibility_of_element_located(PERSONAL_ACCOUNT_LINK))  # Личный кабинет исчез
         logout_success_text = browser.find_element(*LOGIN_BUTTON_MAIN_PAGE).text
-        self.assertEqual(logout_success_text, 'Войти в аккаунт')
+        assert logout_success_text == 'Войти в аккаунт'
