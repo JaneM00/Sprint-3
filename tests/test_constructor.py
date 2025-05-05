4114e694b44232d114f4473e6c196849f6bb32bd
JaneM00 <evg.mamikonyan@yandex.ru>
2025-05-05 12:47:09 +0300

    Update test_constructor.py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,43 +1,54 @@
 # tests/test_constructor.py
 
-import time
-import unittest
+import pytest
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from locators import *
+from locators import MAIN_PAGE_URL, BURGER_LOGO, CONSTRUCTOR_BUNS_SECTION, ACTIVE_TAB_CLASS
 
-class TestConstructor(unittest.TestCase):
 
-    def setUp(self):
-        pass  # Всё управление браузером передано в фикстуру
+@pytest.fixture(scope='session')
+def browser():
+    """Инициализирует веб-драйвер и возвращает объект"""
+    from selenium import webdriver
+    driver = webdriver.Chrome()  # Или другой драйвер по ситуации
+    yield driver
+    driver.quit()
 
-    def tearDown(self):
-        pass  # Всё управление браузером передано в фикстуру
 
-    # Тест открытия раздела "Булки" в конструкторе
+class TestConstructor:
+
+    # Тест открывания секции "Булки" в конструкторе
     def test_open_buns_section(self, browser):
         browser.get(MAIN_PAGE_URL)
         browser.find_element(*BURGER_LOGO).click()
+        
         wait = WebDriverWait(browser, 10)
         wait.until(EC.visibility_of_element_located(CONSTRUCTOR_BUNS_SECTION))
-        active_tab = browser.find_elements_by_css_selector(f'li[data-id="buns"].{ACTIVE_TAB_CLASS}')
-        self.assertGreater(len(active_tab), 0, "Активный таб 'Булки' не найден!")
+        
+        active_tab = browser.find_elements(By.CSS_SELECTOR, f'li[data-id="buns"].{ACTIVE_TAB_CLASS}')
+        assert len(active_tab) > 0, "Активный таб 'Булки' не найден!"
 
-    # Аналогично делаем для остальных разделов
+    # Тест открывания секции "Соус"
     def test_open_sauce_section(self, browser):
         browser.get(MAIN_PAGE_URL)
         browser.find_element(*BURGER_LOGO).click()
+        
         wait = WebDriverWait(browser, 10)
         wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'li[data-id="sauces"]')))
-        browser.find_element_by_css_selector('li[data-id="sauces"]').click()
-        active_tab = browser.find_elements_by_css_selector(f'li[data-id="sauces"].{ACTIVE_TAB_CLASS}')
-        self.assertGreater(len(active_tab), 0, "Активный таб 'Соусы' не найден!")
+        browser.find_element(By.CSS_SELECTOR, 'li[data-id="sauces"]').click()
+        
+        active_tab = browser.find_elements(By.CSS_SELECTOR, f'li[data-id="sauces"].{ACTIVE_TAB_CLASS}')
+        assert len(active_tab) > 0, "Активный таб 'Соусы' не найден!"
 
+    # Тест открывания секции "Начинка"
     def test_open_fillings_section(self, browser):
         browser.get(MAIN_PAGE_URL)
         browser.find_element(*BURGER_LOGO).click()
+        
         wait = WebDriverWait(browser, 10)
         wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'li[data-id="fillings"]')))
-        browser.find_element_by_css_selector('li[data-id="fillings"]').click()
-        active_tab = browser.find_elements_by_css_selector(f'li[data-id="fillings"].{ACTIVE_TAB_CLASS}')
-        self.assertGreater(len(active_tab), 0, "Активный таб 'Начинки' не найден!")
+        browser.find_element(By.CSS_SELECTOR, 'li[data-id="fillings"]').click()
+        
+        active_tab = browser.find_elements(By.CSS_SELECTOR, f'li[data-id="fillings"].{ACTIVE_TAB_CLASS}')
+        assert len(active_tab) > 0, "Активный таб 'Начинки' не найден!"
